745442f5d6c8d26e0b1e34fc245e63737b98138d
imac <busuko@yahoo.com>
2023-11-29 22:32:47 +0200

    add cachalot

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -2,6 +2,8 @@ import datetime
 import time
 from celery_singleton import Singleton
 from celery import shared_task
+from django.conf import settings
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import F
 
@@ -18,6 +20,7 @@ def set_price(subscription_id):
 
         subscription.price = subscription.annotated_price
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 
 @shared_task(base=Singleton)
@@ -29,3 +32,4 @@ def set_comment(subscription_id):
 
         subscription.price = str(datetime.datetime.now())
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
